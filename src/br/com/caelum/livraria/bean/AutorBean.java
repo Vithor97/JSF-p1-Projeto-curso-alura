75670164b3f768dbb0a733de859f1098f910be6e
vithor97 <vitorsantos27@hotmail.com>
2020-12-21 17:18:35 -0300

    first commit

---
+++ src/br/com/caelum/livraria/bean/AutorBean.java
@@ -0,0 +1,27 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
+import br.com.caelum.livraria.util.ForwardView;
+
+@ManagedBean
+public class AutorBean {
+
+	private Autor autor = new Autor();
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public ForwardView gravar() {
+		System.out.println("Gravando autor " + this.autor.getNome());
+
+		new DAO<Autor>(Autor.class).adiciona(this.autor);
+
+		this.autor = new Autor();
+		
+		return new ForwardView("livro");
+	}
+}
